f746e5988e5aefec6dde45c9b78713b3c86bc10e
Олег Маркин <amstloleg@yandex.ru>
2022-10-29 18:08:22 +0700

    Start project. Create book app and model book.

---
+++ cw_read/books/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Book(models.Model):
+    """Модель книги."""
+
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название книги"
+    )
+    author = models.CharField(
+        max_length=150,
+        verbose_name="Автор книги"
+    )
+    description = models.TextField(
+        verbose_name="Описание книги"
+    )
+    cover = models.ImageField(
+        verbose_name="Обложка книги"
+    )
+    genre = models.CharField(
+        max_length=150,
+        verbose_name="Жанр книги"
+    )
+    pub_date = models.DateField(
+        verbose_name="Дата публикации"
+    )
+    num_pages = models.IntegerField(
+        verbose_name="Количество страниц"
+    )
+    add_date = models.DateTimeField(
+        verbose_name="Дата добавления книги на сайт"
+    )
